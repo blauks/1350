65c93b6e3e10d12d800ca966d16abeb62f755299
Jonathan Brooks <brooks.jonathan1@gmail.com>
2020-03-23 22:54:25 +0100

    started on app

---
+++ src/resources/Videos.js
@@ -0,0 +1,36 @@
+const videos = [
+    {
+        id: "2loQBDceatE",
+        title: "Eivind Strøm - Hundekjøring"
+    },
+    {
+        id: "UcgKztGc6cg",
+        title: "Lommedalen Sangkor"
+    },
+    {
+        id: "Zy7CgCC7C4Y",
+        title: "Henrik Hellum - Artist"
+    },
+    {
+        id: "lAZy0FWrdtQ",
+        title: "Et glimt av Lommedalen i sommerdrakt"
+    },
+    {
+        id: "8IBGv1yHNJc",
+        title: "Helena Eide - Dressurrytter"
+    },
+    {
+        id: "0Ts0mE1UBuk",
+        title: "Solveig Torsteinsen - Designer"
+    },
+    {
+        id: "GgFGEsWOg8A",
+        title: "Benjamin Holsten - Frikjører"
+    },
+    {
+        id: "gaHi46sjfNk",
+        title: "Knut Erik Skarning - Historiker"
+    },
+]
+
+export default videos;
