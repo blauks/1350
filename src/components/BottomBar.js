ceb0ac338a03a3785b188206943784371035abc0
Jonathan Brooks <brooks.jonathan1@gmail.com>
2020-03-24 17:16:49 +0100

    new content

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -8,7 +8,41 @@ const BottomBar = props => {
         <div className="lineleft"></div>
         <div className="lineright"></div>
       </div>
-      <div className="bottomBarContent"></div>
+      <div className="bottomBarContent">
+        <div className="bottomBarNettsider">
+          <p className="contentTitle">Våre nettsider:</p>
+          <a
+            href="https://davidbrooksfoto.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            David Brooks
+          </a>
+          <a
+            href="https://morten.brun.no"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Morten Brun
+          </a>
+        </div>
+        <div className="bottomBarSosialeMedier">
+          <p className="contentTitle">Følg oss på sosiale medier:</p>
+          <div className="sosialeMedier">
+            <a
+              href="https://www.facebook.com/1350.no"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={process.env.PUBLIC_URL + 'fbLogo.png'}
+                alt="Facebook logo"
+                className="sosialeMedierLogo"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
